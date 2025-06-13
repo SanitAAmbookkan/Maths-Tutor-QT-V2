d23b4d7b2f48f805839f35816d73156f54efff86
uth-ara <utharakrishnack@gmail.com>
2025-06-13 11:34:23 +0530

    Comments

---
+++ pages/ques_functions.py
@@ -0,0 +1,7 @@
+# 1: Load the Excel file
+# 2: Filter and load only "clicked type" questions
+# 3: Select a random question from the filtered list
+# 4: Show an input box to accept the user's answer
+# 5: Check whether the answer is correct (print the result)
+# 6: If correct, load another question (regardless of difficulty)
+#    Else, repeat the same question

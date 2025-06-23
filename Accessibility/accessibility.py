3d64c475fa201030e17a6dfa8073d0b593457f77
Devikavr24 <devikavrajesh24@gmail.com>
2025-06-23 17:12:22 +0530

    Saving for testing

--- Accessibility/accessibility.py
+++
@@ -1,7 +0,0 @@
-# Accessibility/accessibility.py
-from PyQt5.QtCore import QCoreApplication
-
-def set_accessibility(widget,name, description_key):
-   # tr = QCoreApplication.translate
-    widget.setAccessibleName(name)
-    widget.setAccessibleDescription(description_key)
